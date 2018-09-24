0dff936c07090fea50624f2a6ee494a1ff9eacd8
Rodrigo Varas <rvaras@ceapro.cl>
2018-09-24 16:32:54 -0300

    mqtt ip

--- subsignal1.js
+++ subsignal1.js
@@ -1,7 +1,9 @@
 var SubsignalController = require('./controllers/subsignal');
 
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://192.168.0.8')
+//var client  = mqtt.connect('mqtt://192.168.0.8')
+var client  = mqtt.connect('mqtt://165.227.26.150')
+
 client.on('connect', () => {
     client.subscribe('signal1')
 })
