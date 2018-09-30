99bba4c23224cd49f04e8410d2adaf5082cd5b23
Rodrigo Varas <rvaras@ceapro.cl>
2018-09-30 20:09:51 -0300

    modificacion dist

--- dist/out-tsc/src/app/config/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//export const URL_SERVICIOS = 'http://localhost:3780/';  //para uso local
-//export const URL_SERVICIOS = 'http://142.93.24.11:3789/';
-//export const URL_SERVICIOS = 'https://centropiecmds.cl/';  // para uso servidor con https
-exports.URL_SERVICIOS = 'http://demoadmin.inmobiliariavirtual.cl/';
-//# sourceMappingURL=config.js.map
