70965785dcaa679e8e0fd073f211dfe0744564e9
Rodrigo Varas Urbina <rodrigovarasurbina@iMac-de-Rodrigo.local>
2018-12-17 18:26:32 -0300

    publicar numero aleatorio

--- publisher.js
+++ publisher.js
@@ -1,8 +1,18 @@
 var mqtt = require('mqtt');
-var client  = mqtt.connect('mqtt://165.227.26.150');
+//var client  = mqtt.connect('mqtt://192.168.0.8');
+var client  = mqtt.connect('mqtt://165.227.26.150')
+
 client.on('connect', function () {
 	setInterval(function() {
-		client.publish('postsignalplc001', '789/777');
+		//client.publish('signal1', 'dol8i00001111,aol8i1000i0987i0001i0021i0999i0321i0554i0765,end');
+		var var1 = aleatorio(1,1000);
+		var var2 = aleatorio(1,1000);
+		var resultado =  var1 + '/' + var2;
+		client.publish('postsignalplc003', resultado);
 		//console.log('Message Sent');
 	}, 5000);
 });
+
+function aleatorio(a,b) {
+         return Math.round(Math.random()*(b-a)+parseInt(a));
+}
